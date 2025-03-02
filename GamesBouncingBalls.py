1e609268600786d2624742aab82058ed4ca6ed89
Gnuhq26 <gnuhq26@gmail.com>
2025-03-02 11:14:19 +0700

    completed project

--- GamesBouncingBalls.py
+++ GamesBouncingBalls.py
@@ -1,6 +1,14 @@
 import pygame
 import numpy as np
 import math
+import random
+
+class Ball:
+    def __init__(self, position, velocity):
+        self.pos = np.array(position, dtype = np.float64)
+        self.v = np.array(velocity, dtype = np.float64)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.is_in = True
 
 #ham ve tam giac 
 def draw_arc(window, center, radius, start_angle, end_angle):
@@ -14,6 +22,7 @@ def is_ball_in_arc(ball_pos, CIRCLE_CENTER, start_angle, end_angle):
     dy = ball_pos[1] - CIRCLE_CENTER[1]
     ball_angle = math.atan2(dy, dx)
     start_angle = start_angle % (2 * math.pi)
+    end_angle = end_angle % (2 * math.pi)
     if start_angle > end_angle:
         end_angle += 2 * math.pi
     if start_angle <= ball_angle <= end_angle or (start_angle <= ball_angle + 2 * math.pi <= end_angle):
@@ -39,7 +48,7 @@ arc_degrees = 60
 start_angle = math.radians(-arc_degrees/2)
 end_angle = math.radians(arc_degrees/2)
 spinning_speed = 0.01 
-is_ball_in = True
+balls = [Ball(ball_pos, ball_vel)] 
 
 while running:
     for event in pygame.event.get():
@@ -47,27 +56,34 @@ while running:
             running = False
     start_angle += spinning_speed
     end_angle += spinning_speed
-    ball_vel[1] += GRAVITY
-    ball_pos[0] += ball_vel[0]
-    ball_pos[1] += ball_vel[1]
-    dist = np.linalg.norm(ball_pos - CIRCLE_CENTER)
+    for ball in balls:
+        if ball.pos[1] > HEIGHT or ball.pos[0] < 0 or ball.pos[0] > WIDTH or ball.pos[1] < 0:
+            balls.remove(ball)
+            balls.append(Ball(position=[WIDTH//2, HEIGHT // 2 - 120], velocity=[random.uniform(-4, 4), random.uniform(-1, 1)]))
+            balls.append(Ball(position=[WIDTH//2, HEIGHT // 2 - 120], velocity=[random.uniform(-4, 4), random.uniform(-1, 1)]))
 
-    if dist + BALL_RADIUS > CIRCLE_RADIUS:
-        if is_ball_in_arc(ball_pos, CIRCLE_CENTER, start_angle, end_angle):
-            is_ball_in = False
-        if is_ball_in == True: 
-            d = ball_pos - CIRCLE_CENTER
-            d_unit = d/np.linalg.norm(d)
-            ball_pos = CIRCLE_CENTER + (CIRCLE_RADIUS - BALL_RADIUS) * d_unit
-            t = np.array([-d[1], d[0]], dtype = np.float64) #t la vecto vuong goc voi d
-            proj_v_t = (np.dot(ball_vel, t)/np.dot(t, t)) * t #hinh chieu tu v xuong t
-            ball_vel = 2 * proj_v_t - ball_vel #update van toc bong 
-            ball_vel += t * spinning_speed      # v = r.w, w: toc do quay = spinning_speed
+        ball.v[1] += GRAVITY
+        ball.pos[0] += ball.v[0]
+        ball.pos[1] += ball.v[1]
+        dist = np.linalg.norm(ball.pos - CIRCLE_CENTER)
+
+        if dist + BALL_RADIUS > CIRCLE_RADIUS:
+            if is_ball_in_arc(ball.pos, CIRCLE_CENTER, start_angle, end_angle):
+                ball.is_in = False
+            if ball.is_in == True: 
+                d = ball.pos - CIRCLE_CENTER
+                d_unit = d/np.linalg.norm(d)
+                ball.pos = CIRCLE_CENTER + (CIRCLE_RADIUS - BALL_RADIUS) * d_unit
+                t = np.array([-d[1], d[0]], dtype = np.float64) #t la vecto vuong goc voi d
+                proj_v_t = (np.dot(ball.v, t)/np.dot(t, t)) * t #hinh chieu tu v xuong t
+                ball.v = 2 * proj_v_t - ball.v #update van toc bong 
+                ball.v += t * spinning_speed      # v = r.w, w: toc do quay = spinning_speed
 
     window.fill(BLACK)
     pygame.draw.circle(window, ORANGE, CIRCLE_CENTER, CIRCLE_RADIUS, 3)
     draw_arc(window, CIRCLE_CENTER, CIRCLE_RADIUS, start_angle, end_angle)
-    pygame.draw.circle(window, RED, ball_pos, BALL_RADIUS)
+    for ball in balls:
+        pygame.draw.circle(window, ball.color, ball.pos, BALL_RADIUS)
 
     pygame.display.flip()
     clock.tick(60)
